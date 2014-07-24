8ec5a4e0fe17e2217b7a29911ec0a13a124f37ff
Yasuaki Ohama <yasuaki.ohama@gree.co.jp>
2014-07-24 21:12:28 +0900

    外部templateを使おう

--- ToDo/js/app.js
+++ ToDo/js/app.js
@@ -17,7 +17,7 @@ var TaskView = Backbone.View.extend({
     tagName: 'li',
     //className: 'liClass',
     //id:         'liId'
-    template: _.template("<%- title %>"),
+    template: _.template( $('#task-template').html() ),
     render: function() {
         var template = this.template( this.model.toJSON() );
         this.$el.html(template);
@@ -26,6 +26,6 @@ var TaskView = Backbone.View.extend({
 });
 
 var taskView = new TaskView({ model: task});
-console.log(taskView.render().el);
+$('body').append(taskView.render().el);
 
 })();
